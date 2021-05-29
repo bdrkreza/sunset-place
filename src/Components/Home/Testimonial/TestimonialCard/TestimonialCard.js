0ef4cdea3acad3b9bf8bcfc9c19307b91152b448
Iftekhar Alvee <alveee1998@gmail.com>
2021-05-29 18:36:57 +0600

    check

--- src/Components/Home/Testimonial/TestimonialCard/TestimonialCard.js
+++ src/Components/Home/Testimonial/TestimonialCard/TestimonialCard.js
@@ -12,10 +12,10 @@ const TestimonialCard = () => {
                                 <div class="carousel-item active">
                                     <div class="card border-0 card-0">
                                         <div class="card profile py-3 px-4">
-                                            <div class="text-center"> <img src="https://i.imgur.com/oW8Wpwi.jpg" class="img-fluid profile-pic" /> </div>
+                                            <div class="text-center"> <img src="https://i.imgur.com/oW8Wpwi.jpg" alt="" class="img-fluid profile-pic" /> </div>
                                             <h6 class="mb-0 mt-2">Ximena Vegara</h6> <small>UI/UX Designer</small>
-                                        </div> <img class="img-fluid open-quotes" src="https://i.imgur.com/Hp91vdT.png" width="20" height="20" />
-                                        <p class="content mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod incididunt ut labore et dolore magna aliqua.</p> <img class="img-fluid close-quotes ml-auto" src="https://i.imgur.com/iPcHyJK.png" width="20" height="20" />
+                                        </div> <img alt="" class="img-fluid open-quotes" src="https://i.imgur.com/Hp91vdT.png" width="20" height="20" />
+                                        <p class="content mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod incididunt ut labore et dolore magna aliqua.</p> <img alt="" class="img-fluid close-quotes ml-auto" src="https://i.imgur.com/iPcHyJK.png" width="20" height="20" />
                                     </div>
                                 </div>
                             </div>
